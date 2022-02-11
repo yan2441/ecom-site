d3c01e79c17080c61afb3d696d56c753fed3706e
Yassine najem <yasen2441@gmail.com>
2022-02-11 20:43:03 +0100

    setup the project with depn

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_TOKEN: process.env.SHOPIFY_STOREFRONT_TOKEN,
+  },
+  images: {
+    domains: ['https://cdn.shopify.com'],
+  },
   reactStrictMode: true,
 }
